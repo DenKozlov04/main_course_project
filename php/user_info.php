bbe788dc21c4fcc53d696d4d5b979112ffca72a0
DenKozlov04 <datlovdenis363@gmail.com>
2023-10-05 20:05:51 +0300

    Profile image loading

--- php/user_info.php
+++ php/user_info.php
@@ -1,13 +1,21 @@
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="../css/user_info.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profile</title>
+</head>
+<body>
+    
+</body>
+</html>
 
 <?php
 session_start();
 
 class UserBookings {
-    
     private $mysqli;
-
 
     public function __construct() {
         $this->initializeDatabase();
@@ -27,11 +35,10 @@ class UserBookings {
     }
 
     public function displayUserInfo() {
+        echo '<div id="user-info">';
         echo "<h1>Welcome, {$_SESSION['username']} !</h1>";
         echo "<h1>Your user ID is: {$_SESSION['user_id']}</h1>";
-        echo "<p>Your email is: {$_SESSION['email']}</p>";
-
-        
+        echo "<p>Your email is: {$_SESSION['email']} </p>";
     }
 
     public function getUserBookings() {
@@ -43,12 +50,13 @@ class UserBookings {
         $result = $this->mysqli->query($sql);
 
         if ($result->num_rows > 0) {
+            echo '<div id="bookings-list">';
             // Выводим данные из таблицы
             while ($row = $result->fetch_assoc()) {
                 echo "Booking ID: " . $row["booking_id"] . " - User ID: " . $row["user_id"] . " - Flight ID: " . $row["flight_id"] . " - Booking Date: " . $row["booking_date"] . " - Seat Number: " . $row["seat_number"] . "<br>";
                 echo "<form method='POST' action='user_info.php'>
                         <input type='hidden' name='delete' value='" . $row["user_id"] . "'>
-                        <button type='submit'>Denie</button>
+                        <button  class='delete-button'type='submit'>Denie</button>
                       </form>";
             }
         } else {
@@ -56,10 +64,6 @@ class UserBookings {
         }
     }
 
-
-    
-    
-    
     public function deleteBooking() {
         if (isset($_POST['delete'])) {
             $id = $_POST['delete'];
@@ -70,20 +74,31 @@ class UserBookings {
     public function closeDatabaseConnection() {
         $this->mysqli->close();
     }
+
+    public function displayUserProfileImage() {
+        $user_id = $_SESSION['user_id'];
+        $sql = "SELECT * FROM profile_images WHERE user_id = $user_id";
+        $result = $this->mysqli->query($sql);
+
+        if ($result && $result->num_rows > 0) {
+            $row = $result->fetch_array();
+            $profile_image = $row['profile_image'];//выводим аватар пользователя
+            echo '<img src="data:image/jpeg;base64,' . base64_encode($profile_image) . '" width="200" height="200" />';
+        } else {
+            echo "Изображение не найдено.";
+        }
+    }
 }
 
 $userBookings = new UserBookings();
 $userBookings->displayUserInfo();
 $userBookings->deleteBooking();
 $userBookings->getUserBookings();
+$userBookings->displayUserProfileImage();
 
-
-
-echo "<li><a href='../php/index.php'>Back to main page</a></li>";
+echo "<li><a href='../php/index.php'>Back to the main page</a></li>";
 echo '<form action="upload.php" method="POST" enctype="multipart/form-data">
         <input type="file" name="image" accept="image/*">
         <input type="submit" value="Upload image" name="submit">
       </form>';
 ?>
-
-

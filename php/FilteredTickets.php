e078f2414c6a23de84057e177a79be14bc973886
DenKozlov04 <datlovdenis363@gmail.com>
2023-11-26 17:24:12 +0200

    new card

---
+++ php/FilteredTickets.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../css/FilteredTickets.css">
+    <title>Filtered tickets</title>
+</head>
+<body>
+
+<!-- <div class="img1">
+    <img src="../images/pexels-tanathip-rattanatum-2026324.jpg">
+</div> -->
+<div class="Ticket_box">
+    <div class="Ticket_card">
+        <img src="../images/2000.jpeg">
+        <div class="text1">France</div>
+        <div class="text2">Direct flight</div>
+        <div class="text3">Paris(Paris-Charles-de-Gaulle)</div>
+        <div class="text4"></div>
+        <div class="text5"></div>
+        <div class="text6"></div>
+        <div class="text7"></div>
+        <div class="text8"></div>
+        <a class="BookingBtn"></a>
+    </div>
+</div>
+</div>
+</body>
+</html>
